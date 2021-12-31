01f6b036cdc2e726e15210f0ef6972c9e861088d
Kaushik Gnanaskandan <kaushik.subman@gmail.com>
2021-12-30 20:25:24 -0800

    Not found link

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,14 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Home extends Component {
-    render() {
-        return (
-            <div>
-                <h2>Home</h2>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div>
+        <h2>Home</h2>
+        <Link to="/oops">Let's go</Link>
+      </div>
+    );
+  }
 }
 
 export default Home;
