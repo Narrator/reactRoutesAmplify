dc4604eea40d242f811317cc436f84f717a727e1
Kaushik Gnanaskandan <kaushik.subman@gmail.com>
2022-01-24 20:17:35 -0800

    Add backend with PB

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,9 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "reactroutesamplify": {
+            "GraphQLAPIKeyOutput": "string",
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    }
+}
